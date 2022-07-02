ecff84e2d516e0a303fc31b9fdd80b90763eb331
ArnauRP <arnaurp@gmail.com>
2022-07-02 11:49:39 +0200

    Afegit el material FriccioZero

--- Assets/Scripts/Personatge.cs
+++ Assets/Scripts/Personatge.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class Personatge : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
-    private float _vel;
+    private float _velCorrer;
+    private float _velSaltar;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _vel = 5f;
+        _velCorrer = 5f;
+        _velSaltar = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float inputHoritzontal = Input.GetAxisRaw("Horizontal") * _vel;
+        float inputHoritzontal = Input.GetAxisRaw("Horizontal") * _velCorrer;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _velSaltar);
+        }
         _rigidbody.velocity = new Vector2(inputHoritzontal, _rigidbody.velocity.y);
     }
 }
